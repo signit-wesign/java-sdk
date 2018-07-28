2840e1273ceda3894026581c0d33f6fd6463db51
宁小庚 <572311320@qq.com>
2018-07-28 16:38:13 +0800

    去掉不需要的依赖

--- src/test/java/cn/signit/sdk/SignitClientTest.java
+++ src/test/java/cn/signit/sdk/SignitClientTest.java
@@ -2,15 +2,11 @@ package cn.signit.sdk;
 
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import com.alibaba.fastjson.JSON;
@@ -30,28 +26,21 @@ import cn.signit.sdk.type.AcceptDataType;
 import cn.signit.sdk.type.Direction;
 
 public class SignitClientTest {
-    @Test
+//    @Test
     public void quickSign() throws FileNotFoundException, IOException {
-        // WSID_DEVE_00000164cb192d7d00232444a7d90001
-        File f = new File("E:/quicksign_with_field.pdf");
         Authentication auth = new Authentication();
-        byte[] files = IOUtils.toByteArray(new FileInputStream(f));
-        String signDoc = Base64.encodeBase64String(files);
-        auth.setAppId("e220908f789010fb125b6843f12b303842b9db6b").setSecretKey("sk61fc737afe5d5fa1a5f56a32a4265940");
+        auth.setAppId("appid").setSecretKey("secretKey");
         SignitClient client = new SignitClient(auth);
         FileData fileData = new FileData();
-        fileData.setBase64(signDoc);
-//        fileData.setUrl("https://www.jibbering.com/2002/4/test.txt");
-        String sealbase64 = Base64
-                .encodeBase64String(IOUtils.toByteArray(new FileInputStream(new File("E:/seal.jpg"))));
-        SealData sealData = SealData.builder().withBase64(sealbase64).withName("myseal").build();
+        fileData.setUrl("https://www.jibbering.com/2002/4/test.txt");
+        SealData sealData = SealData.builder().withUrl("http://img.go007.com/2016/03/12/2776ec64f0014006_1.jpg").withName("myseal").build();
         Data data = Data.build().withSealData(sealData).build();
         KeywordPosition keywordPosition = KeywordPosition.builder().withKeyword("page").withRelativeWidthRatio(1.0f)
                 .withRelativeHeightRatio(1.0f).withRelativeOffsetRatio(0.2f).withDirection(Direction.RIGHT)
                 .withScale(1.0f).withPages("all").build();
-        Position position = Position.builder().withFieldName("test_quick_sign").build();
+        Position position = Position.builder().withKeywordPosition(keywordPosition).build();
         SignerInfo signerInfo = SignerInfo.builder().withContact("12345678@qq.com").withName("张三")
-                .withOrgnizationName("宇宙超级无敌有限公司").withIdCardNo("61112197603012136").withLocation("北京市朝阳区")
+                .withOrgnizationName("宇宙超级无敌有限公司").withIdCardNo("61112190005012136").withLocation("北京市朝阳区")
                 .withReason("公司合作").build();
         Signer signer1 = Signer.builder().withData(data).withPosition(position).withSequence(1)
                 .withSignerInfo(signerInfo).build();
