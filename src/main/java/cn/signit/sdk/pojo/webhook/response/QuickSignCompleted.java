818ad5a71ce1e898858a0f3f70853c14036c8b0a
zhangbo <1416694870@qq.com>
2019-04-26 10:07:06 +0800

    1、快捷签署完成事件中增加参数url和base64用于解析数据

--- src/main/java/cn/signit/sdk/pojo/webhook/response/QuickSignCompleted.java
+++ src/main/java/cn/signit/sdk/pojo/webhook/response/QuickSignCompleted.java
@@ -6,5 +6,23 @@ package cn.signit.sdk.pojo.webhook.response;
  * @since 1.0.2
  */
 public class QuickSignCompleted extends AbstractWebhookResponseData {
+    private String url;
+    private String base64;
+
+    public String getBase64() {
+        return base64;
+    }
+
+    public void setBase64(String base64) {
+        this.base64 = base64;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
 }
