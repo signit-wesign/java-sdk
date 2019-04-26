af209492c0bddbc083877ddf602d214727d7eb5c
zhangbo <1416694870@qq.com>
2019-04-26 09:32:14 +0800

    1、新增 个人实名认证提交事件

---
+++ src/main/java/cn/signit/sdk/pojo/webhook/response/PersonVerificationSubmitted.java
@@ -0,0 +1,11 @@
+package cn.signit.sdk.pojo.webhook.response;
+
+/**
+ * 
+ * 个人实名认证提交事件.
+ * 
+ * @since 2.1.0
+ */
+public class PersonVerificationSubmitted extends EnterpriseVerificationPrimaryCompleted {
+
+}
